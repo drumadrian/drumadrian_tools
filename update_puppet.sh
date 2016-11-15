3b70ea557067387cc13a92c2e467b9fdce51d157
pattyr <pattyr@mindtouch.com>
2016-11-15 13:22:22 -0800

    new update puppet file

---
+++ update_puppet.sh
@@ -0,0 +1,88 @@
+##################################################
+#input needed:
+#-path to release directory
+#-name of your workon environment for amazon-aws in puppet-cloud
+##################################################
+
+
+##################################################
+#                                                #
+#   Usage:     pupit IP_Address                  #
+#                                                #
+##################################################
+
+pupit_function() {
+
+    workon_dir="puppet-cloud2-amazon-aws"
+    release_task_path="/Users/pattyramert/Projects/puppet-cloud/mindtouch/amazon-aws/release_tasks"
+    host_param=$1
+    noop=$2
+
+    if [[ -z $host_param ]]; then
+        echo "MISSING HOST"
+        return
+    fi
+
+    if [[ -z $noop ]]; then
+        noop='y'
+    else
+        noop='n'
+    fi
+
+    echo "========================"
+    echo "virtual directory: "$workon_dir
+    echo "release task path: "$release_task_path
+    echo "host param: "$host_param
+    echo "noop: "$noop
+    echo "========================"
+
+	pwd_string=$(pwd)
+    pupit_command='The argument is: '$host_param
+    echo $pupit_command
+
+    workon $workon_dir
+    cd $release_task_path
+    fab puppet_apply:noop=$noop,host=$1
+    deactivate
+    cd $pwd_string
+}
+
+pupit_master_function() {
+
+    workon_dir="puppet-cloud2-amazon-aws"
+    release_task_path="/Users/pattyramert/Projects/puppet-cloud/mindtouch/amazon-aws/release_tasks"
+    host_param=$1
+    noop=$2
+
+    if [[ -z $host_param ]]; then
+        echo "MISSING HOST"
+        return
+    fi
+
+    if [[ -z $noop ]]; then
+        noop='y'
+    else
+        noop='n'
+    fi
+
+    echo "========================"
+    echo "virtual directory: "$workon_dir
+    echo "release task path: "$release_task_path
+    echo "host param: "$host_param
+    echo "noop: "$noop
+    echo "========================"
+
+    pwd_string=$(pwd)
+    pupit_command='The argument is: '$host_param
+    echo $pupit_command
+
+    workon $workon_dir
+    cd $release_task_path
+    fab pm_up
+    fab puppet:noop='y',host=$1
+    deactivate
+    cd $pwd_string
+}
+
+alias update-puppet=pupit_function
+alias update-puppet-master=pupit_master_function
