c320e529f631ceb17c9a3ad2a2d2d039f83befbb
Adrian Drummond <adriand@mindtouch.com>
2016-11-21 20:37:04 -0800

    Updated code to avoid errors and refactored code

--- update_puppet.sh
+++ update_puppet.sh
@@ -6,10 +6,16 @@
 ##################################################
 pupit_function() {
 
-    workon_dir="puppet-cloud2-amazon-aws"
-    release_task_path="/Users/pattyramert/Projects/puppet-cloud/mindtouch/amazon-aws/release_tasks"
+    workon_environment_name="amazon-aws"
+    my_code_dir=$HOME"/code/"
+    release_task_path="puppet-cloud/mindtouch/amazon-aws/release_tasks"
+    complete_release_task_path=$my_code_dir$release_task_path
     host_param=$1
     noop=$2
+
+    # this was added to overcome a problem with running the function in a new shell
+    # The env variables changed when this function was run as a bash scrip
+    source $HOME/.bash_profile
 
     if [[ -z $host_param ]]; then
         echo "MISSING HOST"
@@ -17,35 +23,39 @@ pupit_function() {
     fi
 
     if [[ -z $noop ]]; then
-        noop='y'
-    else
         noop='n'
+    # else
+    #     noop='n'
     fi
 
     echo "========================"
-    echo "virtual directory: "$workon_dir
+    echo "virtual environment name: "$workon_environment_name
     echo "release task path: "$release_task_path
+    echo "complete release task path: "$complete_release_task_path
     echo "host param: "$host_param
     echo "noop: "$noop
     echo "========================"
 
 	pwd_string=$(pwd)
-    pupit_command='The argument is: '$host_param
-    echo $pupit_command
-
-    workon $workon_dir
-    cd $release_task_path
+    workon $workon_environment_name
+    cd "complete_release_task_path"
     fab puppet_apply:noop=$noop,host=$1
     deactivate
-    cd $pwd_string
+    cd "$pwd_string"
 }
 
 pupit_master_function() {
 
-    workon_dir="puppet-cloud2-amazon-aws"
-    release_task_path="/Users/pattyramert/Projects/puppet-cloud/mindtouch/amazon-aws/release_tasks"
+    workon_environment_name="amazon-aws"
+    my_code_dir=$HOME"/code/"
+    release_task_path="puppet-cloud/mindtouch/amazon-aws/release_tasks"
+    complete_release_task_path=$my_code_dir$release_task_path
     host_param=$1
     noop=$2
+
+    # this was added to overcome a problem with running the function in a new shell
+    # The env variables changed when this function was run as a bash scrip
+    source $HOME/.bash_profile
 
     if [[ -z $host_param ]]; then
         echo "MISSING HOST"
@@ -53,29 +63,27 @@ pupit_master_function() {
     fi
 
     if [[ -z $noop ]]; then
-        noop='y'
-    else
         noop='n'
+    # else
+    #     noop='n'
     fi
 
     echo "========================"
-    echo "virtual directory: "$workon_dir
+    echo "virtual environment name: "$workon_environment_name
     echo "release task path: "$release_task_path
+    echo "complete release task path: "$complete_release_task_path
     echo "host param: "$host_param
     echo "noop: "$noop
     echo "========================"
 
     pwd_string=$(pwd)
-    pupit_command='The argument is: '$host_param
-    echo $pupit_command
-
-    workon $workon_dir
-    cd $release_task_path
+    workon $workon_environment_name
+    cd "$complete_release_task_path"
     fab pm_up
-    fab puppet:noop='y',host=$1
+    fab puppet:noop=$noop,host=$1
     deactivate
-    cd $pwd_string
+    cd "$pwd_string"
 }
 
-alias update-puppet=pupit_function
-alias update-puppet-master=pupit_master_function
+alias update-puppet='pupit_function'
+alias update-puppet-master='pupit_master_function'
