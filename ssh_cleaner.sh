becb3c7f489ac0df8681fc84cc582fe96d48b032
Adrian Drummond <adriand@mindtouch.com>
2016-12-21 19:51:40 -0800

    function updates and added global replace

--- ssh_cleaner.sh
+++ ssh_cleaner.sh
@@ -15,72 +15,27 @@ ssh_cleaner() {
         return
     fi
 
-    new_command='echo "ssh $1" | sed 's/_/./' | sed 's/-/./''
+    new_command=`echo ssh $argument_1 | sed 's/_/./g' | sed 's/-/./'g`
 
 
+    echo "========================"
     echo "========================"
     echo "you gave: "$argument_1
     echo "you got: "$new_command
-    echo
-    echo "complete release task path: "$complete_release_task_path
-    echo "host param: "$host_param
-    echo "noop: "$noop
+    echo "========================"
     echo "========================"
 
 
-
+    #execute command
+    $new_command
 }
-
-ssh_repeater() {
-
-    argument_1=$1
-    argument_2=$2
-    argument_3=$3
-    argument_4=$4
-
-    # this was added to overcome a problem with running the function in a new shell
-    # The env variables changed when this function was run as a bash scrip
-    # source $HOME/.bash_profile
-
-    if [[ -z $argument_1 ]]; then
-        echo "MISSING HOST/IP ADDRESS"
-        echo
-        return
-    fi
-
-    echo
-
-
-    echo "========================"
-    echo "you gave: "$argument_1
-    echo "you got: "$release_task_path
-    echo "complete release task path: "$complete_release_task_path
-    echo "host param: "$host_param
-    echo "noop: "$noop
-    echo "========================"
-
-
-
-}
-
 
 
 alias sshc='ssh_cleaner'
-alias sshr='ssh_repeater'
 
 
-
-
-
-
-
-
-sss='echo "ssh $1" | sed 's/_/./' | sed 's/-/./''
-
-
-
-
-echo "ssh $uipadd" | sed 's/_/./' | sed 's/-/./'
+# TEST stream edit
+# echo 52.91.169_78 | sed 's/_/./' | sed 's/-/./'
 
 
 
