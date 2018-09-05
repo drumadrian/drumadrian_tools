f6a8c873ed5fd82cd65ca6a8f043467ca8965271
Adrian Drummond <adrian.drummond@greatcall.com>
2018-09-05 13:54:15 -0700

    adding files

---
+++ userdata-install_apache_and_ssm_amazonlinux_2.bash
@@ -0,0 +1,15 @@
+yum update -y
+yum install -y httpd
+service httpd start
+service httpd start
+mkdir /tmp/ssm
+cd /tmp/ssm
+sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+sudo systemctl status amazon-ssm-agent
+sudo systemctl enable amazon-ssm-agent
+sudo systemctl start amazon-ssm-agent
+sudo systemctl status amazon-ssm-agent
+
+
+
+
