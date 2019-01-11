0ac3b0e4657daf782f766ab2ff89b2354e12b99b
Adrian Drummond <adrian.drummond@greatcall.com>
2019-01-11 14:27:19 -0800

    adding update to script

--- userdata-install_apache_and_ssm_amazonlinux_2.bash
+++ userdata-install_apache_and_ssm_amazonlinux_2.bash
@@ -1,7 +1,7 @@
 yum update -y
 yum install -y httpd
 service httpd start
-service httpd start
+service httpd status
 mkdir /tmp/ssm
 cd /tmp/ssm
 sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
@@ -12,4 +12,6 @@ sudo systemctl status amazon-ssm-agent
 
 
 
+
+# https://docs.aws.amazon.com/systems-manager/latest/userguide/sysman-manual-agent-install.html#agent-install-al
 
